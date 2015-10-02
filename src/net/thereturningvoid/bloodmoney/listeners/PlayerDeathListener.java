cf862f746920e8fe9018125cefbe02c90d96c572
TheReturningVoid <jacobjuric1@gmail.com>
2015-10-02 21:38:42 +1000

    Players now lose money on death (toggleable)

--- src/net/thereturningvoid/bloodmoney/listeners/PlayerDeathListener.java
+++ src/net/thereturningvoid/bloodmoney/listeners/PlayerDeathListener.java
@@ -4,6 +4,7 @@ import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
 import net.milkbowl.vault.permission.Permission;
 import net.thereturningvoid.bloodmoney.BloodMoney;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,7 +12,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
 import java.util.Map;
-import java.util.regex.Matcher;
 
 public class PlayerDeathListener implements Listener {
 
@@ -26,22 +26,33 @@ public class PlayerDeathListener implements Listener {
     }
 
     @EventHandler
-    public void onKillPlayer(PlayerDeathEvent e) {
+    public void onKillPlayer(PlayerDeathEvent e) throws InvalidConfigurationException {
         if (e.getEntityType() == EntityType.PLAYER) {
             Player pKilled = e.getEntity();
             Player pKilledBy = pKilled.getKiller();
             if (getRanks() != null) {
-                if (plugin.getConfig().getBoolean("bloodmoney.sendMessageToDeadPlayer")) {
+                if (plugin.getConfig().getBoolean("bloodmoney.sendMessageToDeadPlayer") && !plugin.getConfig().getBoolean("bloodmoney.loseMoneyOnDeath")) {
                     String dMessage = plugin.getConfig().getString("bloodmoney.deathMessage").replace("%p", pKilledBy.getName());
                     pKilled.sendMessage(BloodMoney.CHAT_PREFIX + dMessage);
                 }
                 for (Map.Entry rank : getRanks().entrySet()) {
                     String rankName = (String) rank.getKey();
                     if (perm.has(pKilledBy, "bloodmoney.rank." + rank.getKey())) {
-                        EconomyResponse creditPlayer = econ.depositPlayer(pKilledBy, plugin.getConfig().getDouble("bloodmoney.ranks." + rankName));
+                        EconomyResponse creditPlayer = econ.depositPlayer(pKilledBy, plugin.getConfig().getDouble("bloodmoney.ranks." + rankName + ".gain"));
                         if (creditPlayer.transactionSuccess()) {
-                            String kMessage = plugin.getConfig().getString("bloodmoney.killMessage").replace("%m", econ.format(creditPlayer.amount)).replace("%p", Matcher.quoteReplacement(pKilled.getName()));
+                            String kMessage = plugin.getConfig().getString("bloodmoney.killMessage").replace("%m", econ.format(creditPlayer.amount)).replace("%p", pKilled.getName());
                             pKilledBy.sendMessage(BloodMoney.CHAT_PREFIX + kMessage);
+                        } else {
+                            pKilledBy.sendMessage(BloodMoney.CHAT_PREFIX + "Something went wrong and the transaction was unsuccessful.");
+                        }
+                    }
+                    if (plugin.getConfig().getBoolean("bloodmoney.loseMoneyOnDeath")) {
+                        if (perm.has(pKilled, "bloodmoney.rank." + rank.getKey())) {
+                            EconomyResponse takeFromPlayer = econ.withdrawPlayer(pKilled, plugin.getConfig().getDouble("bloodmoney.ranks." + rankName + ".loss"));
+                            if (takeFromPlayer.transactionSuccess()) {
+                                String kMessage2 = plugin.getConfig().getString("bloodmoney.deathMessage").replace("%p", pKilledBy.getName()).replace("%m", econ.format(takeFromPlayer.amount));
+                                pKilled.sendMessage(BloodMoney.CHAT_PREFIX + kMessage2);
+                            }
                         }
                     }
                 }
